b600d3d23b0166d583b4e7d2465b5d40b59b10f0
Tom Nielsen <tanielsen@gmail.com>
2017-11-20 22:53:59 +0000

    hasseq

--- lib/Pandata/Types.hs
+++ lib/Pandata/Types.hs
@@ -10,6 +10,7 @@ import qualified Data.Set as Set
 import Data.Set (Set)
 import qualified Streaming.Prelude as S
 import Streaming.Prelude (Stream, Of)
+import Data.ByteString (ByteString)
 
 
 
@@ -23,7 +24,6 @@ data T = TCon Text -- primitives
        | Nullable T
   deriving (Generic, Show, Read, Eq)
 
--- we could probably just have used Data.Dynamic.
 data V = VInt Int | VBool Bool | VDouble Double | VText Text
        | VEnumText Text --if we dont yet know type
        | VEnumTag Word8
@@ -33,9 +33,18 @@ data V = VInt Int | VBool Bool | VDouble Double | VText Text
        | VSum Int V
        | VProd [V]
        | VNull
+       | VBS ByteString
 
-
-
+--cannot be represented as a ByteString
+hasSeq :: T -> Bool
+hasSeq (Sequence _) = True
+hasSeq (Array t) = hasSeq t
+hasSeq (Nullable t) = hasSeq t
+hasSeq (Rec kts) = any hasSeq $ Map.elems kts
+hasSeq (Sum ts) = any hasSeq ts
+hasSeq (Prod ts) = any hasSeq ts
+hasSeq (Enum _) = False
+hasSeq (TCon _) = False
 
 data Portal ds = Portal
   {
